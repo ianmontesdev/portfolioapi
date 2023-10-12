e27ac948b323be53cc63105a407e8def8f630095
ianmontesdev <fotoianmontes@gmail.com>
2023-10-12 15:35:41 +0100

    Modified code model to return assets in response

--- src/main/java/com/ianmontes/dev/portfolioapi/Code.java
+++ src/main/java/com/ianmontes/dev/portfolioapi/Code.java
@@ -1,5 +1,6 @@
 package com.ianmontes.dev.portfolioapi;
 
+import com.fasterxml.jackson.annotation.JsonRawValue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,8 @@ import java.util.List;
 public class Code {
     private String title;
     private String profilePic;
+    @JsonRawValue
+    private String assets;
     private List<String> techStack;
     private List<String> education;
     private List<String> experience;
